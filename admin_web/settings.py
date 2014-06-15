0634b2f112e3c1e90bd3ad11c195e06cbf754f8e
djkram <marc.planaguma@gmail.com>
2014-06-15 23:02:55 +0200

    debug false

--- admin_web/settings.py
+++ admin_web/settings.py
@@ -12,7 +12,7 @@ DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
-    # ('Your Name', 'your_email@example.com'),
+     ('Marc Planaguma', 'mplanaguma@bdigital.org'),
 )
 
 MANAGERS = ADMINS
@@ -29,6 +29,10 @@ DATABASES = {
 }
 
 #SPATIALITE_LIBRARY_PATH='/Library/Frameworks/SQLite3.framework/SQLite3'
+
+# Hosts/domain names that are valid for this site; required if DEBUG is False
+# See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
+ALLOWED_HOSTS = ['lleida.opendai.eu', 'bcn.opendai.eu']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -157,7 +161,7 @@ LEAFLET_CONFIG = {
 }
 
 # Celery Broker
-#BROKER_URL = 'amqp://guest:guest@localhost:5672/' #RabbiMQ
+BROKER_URL = 'amqp://guest:guest@localhost:5672/' #RabbiMQ
 #BROKER_URL = 'django://' #Django ORM
 #BROKER_HOST = "localhost"
 #BROKER_PORT = 5672
@@ -165,13 +169,14 @@ LEAFLET_CONFIG = {
 #BROKER_PASSWORD = "20272027"
 #BROKER_VHOST = "testhost"
 #CELERY_SEND_TASK_SENT_EVENT = True
-CELERY_ALWAYS_EAGER = True
+#CELERY_ALWAYS_EAGER = True
 
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
 # the site admins on every HTTP 500 error when DEBUG=False.
 # See http://docs.djangoproject.com/en/dev/topics/logging for
 # more details on how to customize your logging configuration.
+'''
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
@@ -193,5 +198,79 @@ LOGGING = {
             'level': 'ERROR',
             'propagate': True,
         },
+    }
+}
+'''
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '[%(levelname)s %(asctime)s %(thread)d] %(module)s:%(funcName)s - %(message)s'
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+        },
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'class': 'django.utils.log.AdminEmailHandler'
+        },    
+        'null': {
+            'level': 'DEBUG',
+            'class': 'django.utils.log.NullHandler'
+        }, 
+        'console':{
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple'
+        },
+        'logfile': {
+            'level':'INFO',
+            'filters': ['require_debug_false'],
+            'class':'logging.handlers.RotatingFileHandler',
+            'filename' : '/var/log/opendai/django/django.log',
+            'maxBytes': 1024*1024*50, # 50MB
+            'backupCount': 5,
+            'formatter': 'verbose',
+        }        
+    },
+    'loggers': {
+        'urbevo_api': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': False
+        },
+        'urbevo_server': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': False
+        },
+        'urbevo_ui': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': False
+        },                
+        'celery': {
+            'handlers': ['console'],
+            'level': 'INFO',
+            'propagate': True
+        },
+        'django': {
+            'handlers': ['console', 'logfile'],
+            'level': 'INFO',
+            'propagate': True
+        },
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+        }
     }
 }
