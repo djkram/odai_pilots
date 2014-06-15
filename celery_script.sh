1523e514dccc84cad86b4e2a21ee5766afad555d
djkram <marc.planaguma@gmail.com>
2014-06-15 23:09:28 +0200

    celery script

--- celery_script.sh
+++ celery_script.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 set -e
 
-DJANGO_SETTINGS_MODULE=mmpp_conf.settings.production
+DJANGO_SETTINGS_MODULE=admin_web.settings.production
 
-LOGFILE=~/logs/mmpp_service-celery-stdout.log
-LOGFILEERR=~/logs/mmpp_service-celery-stderr.log
+LOGFILE=/var/log/opendai/django_console.log
 LOGDIR=$(dirname $LOGFILE)
 NUM_WORKERS=3
 
 # user/group to run as
-USER=c2020
-GROUP=c2020
-cd ~/C2020Clustering/mmpp_service
-source ../env/bin/activate
+USER=root
+GROUP=root
+cd /var/www/odai_pilots
+#source ../env/bin/activate
 
 test -d $LOGDIR || mkdir -p $LOGDIR
-exec python manage.py celery worker -l INFO 2>>$LOGFILEERR
+exec python manage.py celery worker -l INFO 2>>$LOGFILE
