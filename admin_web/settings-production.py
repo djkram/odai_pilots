ad2787ccb229971c12668f087439df6eff3a9b4f
djkram <marc.planaguma@gmail.com>
2014-06-17 15:16:48 +0200

    ip 194.116.110.155 as allowed host

--- admin_web/settings-production.py
+++ admin_web/settings-production.py
@@ -32,7 +32,7 @@ DATABASES = {
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ['lleida.opendai.eu', 'bcn.opendai.eu']
+ALLOWED_HOSTS = ['lleida.opendai.eu', 'bcn.opendai.eu', '194.116.110.155']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
